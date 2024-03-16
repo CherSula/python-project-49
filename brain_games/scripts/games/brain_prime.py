70a7708fc162e99cbb17f64362c43c9ae1bb0a12
CherSula <chers1303@gmail.com>
2024-03-16 23:37:44 +0400

    9th step

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -6,17 +6,24 @@ def start_round():
     gen_number = randint(1, 100)
     answer = prompt.string(f'Question: {gen_number}\nYour answer: ')
     if answer == 'yes':
-        if (gen_number != 1
-                and gen_number % gen_number == 0
-                and gen_number % 1 == gen_number):
-            return 1
+        if gen_number > 1:
+            deviders = 0
+            for number in range(2, gen_number // 2 + 1):
+                if (gen_number % number == 0):
+                    deviders += 1
+            if (deviders <= 0):
+                return 1
         else:
             print(f'"{answer}" is wrong answer ;(. Correct answer was "no".')
             return 0
     elif answer == 'no':
-        if (gen_number == 1
-                or gen_number % gen_number != 0
-                or gen_number % 1 != gen_number):
+        deviders = 0
+        for number in range(2, gen_number // 2 + 1):
+            if (gen_number % number == 0):
+                deviders += 1
+        if (deviders > 0):
+            return 1
+        elif gen_number == 1:
             return 1
         else:
             print(f'"{answer}" is wrong answer ;(. Correct answer was "yes".')
